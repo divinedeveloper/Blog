43c27d5c7f17509d665537b05aedb743d925ba73
Swapnil C. Sawant <sawant.swapnil1@gmail.com>
2014-03-29 15:15:22 +0530

    Adding routes for tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Blog::Application.routes.draw do
 	devise_for :users
 
 	get 'posts/page/:page' => 'posts#index'
+	get 'tags/:tag', to: 'posts#tagged', as: :tag
+	
+	match 'tagged', to: 'posts#tagged', :as => 'tagged', via: 'get' 
 	resources :posts
 
 	root to: "welcome#index"
