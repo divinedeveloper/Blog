d5c93ac55382d704ae47e59e2ed64a7df13241d6
Swapnil C. Sawant <sawant.swapnil1@gmail.com>
2014-03-30 16:12:10 +0530

    Schema changed and using paperclip in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,16 @@ class Post < ActiveRecord::Base
   belongs_to :user
   acts_as_taggable
   acts_as_taggable_on :tags
+  has_attached_file :avatar, 
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/style/missing_:style.png"
+  validates_attachment :avatar,
+                       :presence => true,
+                       :content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/ },
+                       :size => { :in => 0..1.megabytes }
 
   validates :title, presence: true, 
   					length: { minimum: 5 }
+
+  validates_presence_of :tag_list  
 end
